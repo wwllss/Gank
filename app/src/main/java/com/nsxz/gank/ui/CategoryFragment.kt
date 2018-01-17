623ae7e3f719ab1b2ab7777e76cb45bf045c88b8
zhangyuan <fromngatoo@gmail.com>
2018-01-17 21:50:42 +0800

    UI

--- app/src/main/java/com/nsxz/gank/ui/CategoryFragment.kt
+++ app/src/main/java/com/nsxz/gank/ui/CategoryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -82,7 +83,7 @@ class CategoryFragment : Fragment() {
             }
             holder.itemView.apply {
                 desc.text = category.desc
-                provider.text = category.who
+                provider.text = if (TextUtils.isEmpty(category.who)) "匿名" else category.who
                 time.text = category.publishedAt
             }
         }
